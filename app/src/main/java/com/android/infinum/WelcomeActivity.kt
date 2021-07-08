59d72a97638f9123c7df43483750f8634efb994c
mihaelrodek <mihael.rodek1@gmail.com>
2021-07-09 01:20:41 +0200

    Dodani layouti i activityi prije koristenja figme

---
+++ app/src/main/java/com/android/infinum/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.android.infinum
+
+import android.app.Activity
+import android.os.Bundle
+
+class WelcomeActivity : Activity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
