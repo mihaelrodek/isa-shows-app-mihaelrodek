16dd961a08e975c2b7f396a9028085e837ddda31
mihaelrodek <mihael.rodek1@gmail.com>
2021-07-26 12:02:57 +0200

    Promjenjen login screen

--- app/src/main/java/com/android/infinum/LoginActivity.kt
+++ app/src/main/java/com/android/infinum/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.android.infinum
 
 import android.app.Activity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Patterns
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -12,8 +14,19 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
-    companion object{
-        private const val pwRegex = """^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}${'$'}"""
+    private val textWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            binding.loginButton.isEnabled=checkPassword(binding.passwordInput.editText?.text.toString()) &&
+                    checkMail(binding.emailInput.editText?.text.toString())
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+
+        }
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,35 +35,44 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.loginButton.isClickable = false
 
-        if (binding.emailInput.editText?.text.toString().isNotEmpty() &&
-            binding.passwordInput.editText?.text.toString().isNotEmpty())
-            binding.loginButton.isClickable = true
+        binding.passwordInputEditor.addTextChangedListener(textWatcher)
+        binding.emailInputEditor.addTextChangedListener(textWatcher)
+
 
         binding.loginButton.setOnClickListener {
 
-            checkMailAndPassword(binding.emailInput.editText?.text.toString(),binding.passwordInput.editText?.text.toString())
-
-        }
-
-    }
-
-    private fun checkMailAndPassword(email: String, password: String) {
-
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches())
-            Toast.makeText(this, "Wrong email. Please try again.", Toast.LENGTH_SHORT).show()
-        else if(!pwRegex.toRegex().containsMatchIn(password)){
-            Toast.makeText(this, "Wrong password format. Password should contain one capital letter, one lower letter and one number.", Toast.LENGTH_SHORT).show()
-        }
-        else {
             val intent =
                 WelcomeActivity.buildIntent(
                     this,
                     binding.emailInput.editText?.text.toString()
                 )
             startActivity(intent)
+
         }
+    }
+
+    private fun checkMail(email: String) : Boolean {
+
+        if (!email.contains("@")|| !Patterns.EMAIL_ADDRESS.matcher(email).matches() || email.isEmpty()) {
+            Toast.makeText(this, "email", Toast.LENGTH_SHORT).show()
+            binding.emailInput.error = "Please enter valid e-mail."
+            return false
+        }else
+            binding.emailInput.error= null
+        return true
+    }
+
+    private fun checkPassword(password : String) : Boolean {
+
+        if (!password.length?.let { len -> len >= 6 }) {
+            println(password)
+
+            binding.passwordInput.error = "Password should be at least 6 characters long."
+            return false
+        }else
+            binding.passwordInput.error= null
+        return true
     }
 
 }
